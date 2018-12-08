81059b17f6727adb3195a9e3949ae7695a08fc0f
RabbieW <45704802+RabbieW@users.noreply.github.com>
2018-12-08 13:09:19 +0800

    Add files via upload

---
+++ home programme 5.cpp
@@ -0,0 +1,23 @@
+//carrots.cpp -- food processing programme
+//uses and displays a variable
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+	
+	int carrots;
+	
+	cout << "carrots = ";
+	cin >> carrots;
+	cout << "I have "
+	     << carrots
+		 << " carrots"
+		 << endl;
+	carrots -= 1;
+	cout << "Crunch, crunch, now I have  "
+	     << carrots
+		 << " carrots."
+		 << endl;
+}
